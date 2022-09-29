6b2ec7132a07138521f4595fa4a260221a926494
filipecardoso0 <filipe12200@gmail.com>
2022-09-29 14:26:42 +0100

    TP1 files added

---
+++ TP1/write_noncanonical.c
@@ -0,0 +1,154 @@
+// Write to serial port in non-canonical mode
+//
+// Modified by: Eduardo Nuno Almeida [enalmeida@fe.up.pt]
+
+#include <fcntl.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <termios.h>
+#include <unistd.h>
+
+// Baudrate settings are defined in <asm/termbits.h>, which is
+// included by <termios.h>
+#define BAUDRATE B38400
+#define _POSIX_SOURCE 1 // POSIX compliant source
+
+#define FALSE 0
+#define TRUE 1
+
+#define BUF_SIZE 256
+
+volatile int STOP = FALSE;
+
+int main(int argc, char *argv[])
+{
+    // Program usage: Uses either COM1 or COM2
+    const char *serialPortName = argv[1];
+
+    if (argc < 2)
+    {
+        printf("Incorrect program usage\n"
+               "Usage: %s <SerialPort>\n"
+               "Example: %s /dev/ttyS1\n",
+               argv[0],
+               argv[0]);
+        exit(1);
+    }
+
+    // Open serial port device for reading and writing, and not as controlling tty
+    // because we don't want to get killed if linenoise sends CTRL-C.
+    int fd = open(serialPortName, O_RDWR | O_NOCTTY);
+
+    if (fd < 0)
+    {
+        perror(serialPortName);
+        exit(-1);
+    }
+
+    struct termios oldtio;
+    struct termios newtio;
+
+    // Save current port settings
+    if (tcgetattr(fd, &oldtio) == -1)
+    {
+        perror("tcgetattr");
+        exit(-1);
+    }
+
+    // Clear struct for new port settings
+    memset(&newtio, 0, sizeof(newtio));
+
+    newtio.c_cflag = BAUDRATE | CS8 | CLOCAL | CREAD;
+    newtio.c_iflag = IGNPAR;
+    newtio.c_oflag = 0;
+
+    // Set input mode (non-canonical, no echo,...)
+    newtio.c_lflag = 0;
+    newtio.c_cc[VTIME] = 1; // Inter-character timer unused
+    newtio.c_cc[VMIN] = 0;  // Blocking read until 5 chars received
+
+    // VTIME e VMIN should be changed in order to protect with a
+    // timeout the reception of the following character(s)
+
+    // Now clean the line and activate the settings for the port
+    // tcflush() discards data written to the object referred to
+    // by fd but not transmitted, or data received but not read,
+    // depending on the value of queue_selector:
+    //   TCIFLUSH - flushes data received but not read.
+    tcflush(fd, TCIOFLUSH);
+
+    // Set new port settings
+    if (tcsetattr(fd, TCSANOW, &newtio) == -1)
+    {
+        perror("tcsetattr");
+        exit(-1);
+    }
+
+    printf("New termios structure set\n");
+
+    // Create string to send
+    unsigned char buf[BUF_SIZE] = {0};
+
+    //Reads string from stdin
+    printf("Please enter the message you want to send: \n"); 
+
+    //Writes the user input in the stdin and inserts the null terminating byte
+    gets(buf); 
+
+    //Determines number of characters read until '/0' -> String Terminating Byte
+    int numcharsread = 0; 
+    numcharsread = strlen(buf);
+
+    // In non-canonical mode, '\n' does not end the writing.
+    // Test this condition by placing a '\n' in the middle of the buffer.
+    // The whole buffer must be sent even with the '\n'.
+    //buf[5] = '\n';
+
+    // Writes all the string bytes to the fd (Serial Port) including the null terminating byte
+    int bytes = write(fd, buf, numcharsread+1);
+    printf("%d bytes written\n", bytes);
+
+    // Wait until all bytes have been written to the serial port
+    sleep(1);
+
+    unsigned char receptbuf[BUF_SIZE + 1] = {0}; // +1: Save space for the final '\0' char
+
+    int bytecnt = 0; 
+    
+    //Read from the serial port until \0
+    while (STOP == FALSE){
+        int bytes = read(fd, buf+bytecnt, 1);
+        if(bytes > 0){
+            if(buf[bytecnt] == '\0'){
+                STOP = TRUE;
+                //Adds null terminating byte
+                buf[bytecnt] = '\0'; 
+                bytecnt++;  
+            }
+            else
+                bytecnt++; 
+        }
+    }
+ 
+    //Prints in the stdout the string read from the serial port
+   if(STOP == TRUE){
+        printf("Message read from serial port (Listener): %s \n", buf); 
+    }
+
+    // Wait until all bytes have been read from the serial port
+    sleep(1); 
+
+    // Restore the old port settings
+    if (tcsetattr(fd, TCSANOW, &oldtio) == -1)
+    {
+        perror("tcsetattr");
+        exit(-1);
+    }
+
+    close(fd);
+
+    return 0;
+}
